2404167cc9720fd04cdf3bc28aafdcc892562fa1
Tanja Rietveldt <diafox2015@gmail.com>
2021-01-24 15:41:55 +0100

    add urls's of allauth to the menu

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -23,7 +23,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('',TemplateView.as_view(template_name='home.html')),
+    path('',TemplateView.as_view(template_name='home.html'),name="home"),
     path('books/',include('books.urls'))
 ]
 if settings.DEBUG:

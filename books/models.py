e69e0f9518242632f5a07814b5fa229eb1fc4011
Tanja Rietveldt <diafox2015@gmail.com>
2021-01-24 21:27:47 +0100

    set mptt for category model

--- books/models.py
+++ books/models.py
@@ -1,10 +1,22 @@
-# from django.db import models
+from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
+
 # from django.shortcuts import reverse
-#
-#
-# class Category(models.Model):
-#     name = models.CharField(max_length=250)
-#
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=100, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    class Meta:
+        verbose_name_plural = "Categories"
+    def __str__(self):
+        return self.name
+
+    #
 #
 # class Tag(models.Model):
 #     name = models.CharField(max_length=60)
